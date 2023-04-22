c5a097b4242d1deff550d60cbb31a96789d0cdff
callumscottward <callumward@unomaha.edu>
2023-04-21 21:04:52 -0500

    Updated account.py to match test branch and removed docstrings + type indicators

--- account.py
+++ account.py
@@ -1,18 +1,9 @@
 class Account:
     def __init__(self, name):
-        """
-        Function to set up account object
-        :param name: Account name
-        """
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount) -> bool:
-        """
-        Function to add funds to account
-        :param amount: Amount of dollars to add
-        :return: Boolean representing success of deposit or lack thereof
-        """
+    def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
             return True
@@ -20,27 +11,14 @@ class Account:
             return False
 
     def withdraw(self, amount):
-        """
-        Function to take funds out of account
-        :param amount: Amount of dollars to remove
-        :return: Boolean representing success of withdrawal or lack thereof
-        """
-        if 0 < amount <= self.__account_balance:
+        if amount > 0 and amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
             return False
 
     def get_balance(self):
-        """
-        Function to retrieve the private value of balance on account
-        :return: Balance in dollars
-        """
         return self.__account_balance
 
     def get_name(self):
-        """
-        Function to retrieve the private value of name on account
-        :return: Name of account
-        """
         return self.__account_name
